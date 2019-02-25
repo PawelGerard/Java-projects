837c4d461e0e6294eb97d2fd16b6b0d23ac86dc9
PawelGerard <47610591+PawelGerard@users.noreply.github.com>
2019-02-25 19:25:56 +0100

    Add files via upload

---
+++ Example/src/Main/Main.java
@@ -0,0 +1,71 @@
+package Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.SilentCssErrorHandler;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlImage;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		Map<Integer, String> map = new HashMap<>();
+		map.va
+	
+	}
+	
+	public static boolean isPrime(int x) {
+		
+		for (int i = 2;i<x;i++) {
+			if (x % i == 0)
+			return false;
+		}
+	 return true;
+	}
+}		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+
+
